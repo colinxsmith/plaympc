bdb8806d71c14251ed625f5be9eb351298155b57
Colin <colinxsmith@gmail.com>
2025-01-27 23:08:15 +0000

    .

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export function App() {
           <img src="http://192.168.0.37:1234/ak.jpg" className="App-logo" alt="logo" ></img>
         </div>
       </header>
-      <svg>
+      <svg >
         <line x1="20px" x2="200px" y1="20px" y2="200px" class="tester">
           <title>Colin</title>
         </line>
