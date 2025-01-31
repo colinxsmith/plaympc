c6e63829deaa6c7237a4c5be604e27c1ba4476a7
Colin <colinxsmith@gmail.com>
2025-01-31 00:11:50 +0000

    .

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export function App() {
           <img src="http://192.168.0.37:1234/ak.jpg" className="App-logo" alt="logo" ></img>
         </div>
       </header>
-      <PlayN station={4}></PlayN>
+      <PlayN station={3}></PlayN>
     </div>
 
   );
--- src/PlayN.js
+++ src/PlayN.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import './PlayN.css'
-export const PlayN=({station})=>{
-    React.useEffect(() => {
-       fetch(`http://192.168.0.37:1310/dave?value=${station}`,{
-         mode: 'no-cors',})
-          .then((res) => res.json())
-          .then((data) => {
-             console.log(data);
-          })
-          .catch((err) => {
-             console.log(err.message);
-          });
-    }, [station]);
- 
-    return (
-        <div>
-            <p  className='backers' title='station'>Trying to get station number {station}</p>
-        </div>
-    );
- };
+export const PlayN = ({ station }) => {
+  const [backvalue, setB] = React.useState([]);
+  React.useEffect(() => {
+    const headers = { 'Content-Type': 'application/json' }
+    const url = `http://192.168.0.37:1310/dave?value=${station}`;
+    fetch(url, {
+      mode: 'no-cors'
+    })
+      .then((rs) => (rs.json()))
+      .then((ss) => setB(ss))
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, [station]);
+
+  return (
+    <div>
+      <p className='backers' title='station'>Trying to get station number {station}</p>
+      {
+        backvalue.map((i) => (
+          <p className='backers' title='received'>{i} </p>
+        ))
+      }
+    </div>
+  );
+};
