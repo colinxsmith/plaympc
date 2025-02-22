03b81b1b19e58c0ba53d02ad94baf9b5e3253932
Colin <colinxsmith@gmail.com>
2025-02-22 21:18:22 +0000

    New outline

--- src/App.js
+++ src/App.js
@@ -4,33 +4,33 @@ import React from 'react';
 
 export function App() {
   const [programnumber, setProgramNumber] = React.useState('-1');
-  const [mp3number,setMp3Number]=React.useState(undefined);
-  var [deletenumber,setDeleteNumber]=React.useState('-1');
-  const [rescan,setRescan]=React.useState(false);
+  const [mp3number, setMp3Number] = React.useState('-1');
+  var [deletenumber, setDeleteNumber] = React.useState('-1');
+  const [rescan, setRescan] = React.useState(false);
   var [seek, setSeek] = React.useState('-1');
   const url = `http://192.168.0.37:1310`;
-  var prog=-1;
-  if(seek===undefined){
-    seek=-1;
+  var prog = '-1';
+  if (seek === undefined) {
+    seek = -1;
   }
-  if(programnumber!==undefined){
+  if (programnumber !== -1) {
     console.log(programnumber)
-    prog=programnumber.split(' ')[0];
+    prog = programnumber.split(' ')[0];
   }
-  if(deletenumber!=='-1')deletenumber  =deletenumber.split(' ')[0];
-  console.log(seek,prog,mp3number,deletenumber,rescan)
-  var urlpart='?';
-  if(prog!==-1)urlpart+='value='+prog;
-  else if(seek!==-1)urlpart+='seek='+seek;
-  else if(mp3number!==undefined)urlpart+='mp3='+mp3number;
-  else if(deletenumber!=='-1')urlpart+='remove'+deletenumber;
-  else if(rescan)urlpart+='rescan';
+  if (deletenumber !== '-1') deletenumber = deletenumber.split(' ')[0];
+  console.log(seek, prog, mp3number, deletenumber, rescan)
+  var urlpart = '?';
+  if (prog !== '-1') urlpart += 'value=' + prog;
+  else if (seek !== -1) urlpart += 'seek=' + seek;
+  else if (mp3number !== '-1') urlpart += 'mp3=' + mp3number;
+  else if (deletenumber !== '-1') urlpart += 'remove' + deletenumber;
+  else if (rescan) urlpart += 'update';
 
-  if(prog!==-1 && !urlpart.includes('value'))urlpart+='&value='+prog;
-  if(seek!==-1 && !urlpart.includes('seek'))urlpart+='&seek='+seek;
-  if(mp3number!==undefined && !urlpart.includes('mp3'))urlpart+='&mp3='+mp3number;
-  if(deletenumber!=='-1' )urlpart+='&remove='+deletenumber;
-  if(rescan )urlpart+='&rescan';
+  if (prog !== '-1' && !urlpart.includes('value')) urlpart += '&value=' + prog;
+  if (seek !== -1 && !urlpart.includes('seek')) urlpart += '&seek=' + seek;
+  if (mp3number !== '-1' && !urlpart.includes('mp3')) urlpart += '&mp3=' + mp3number;
+  if (deletenumber !== '-1') urlpart += '&remove=' + deletenumber;
+  if (rescan) urlpart += '&update';
 
   console.log(urlpart)
   return (
@@ -40,8 +40,8 @@ export function App() {
           <img src="http://192.168.0.37:1234/ak.jpg" className="App-logo" alt="logo" ></img>
         </div>
       </header>
-      <PlayN deletenumber={deletenumber} setDeleteNumber={setDeleteNumber} rescan={rescan} setRescan={setRescan} mp3number={mp3number}  setMp3Number={setMp3Number} programnumber={programnumber} setProgramNumber={setProgramNumber} seek={seek} setSeek={setSeek} url={url}></PlayN>
-      <PlayN deletenumber={deletenumber} setDeleteNumber={setDeleteNumber}  rescan={rescan} setRescan={setRescan}  mp3number={mp3number}  setMp3Number={setMp3Number} programnumber={programnumber} setProgramNumber={setProgramNumber} seek={seek} setSeek={setSeek} url={url+'/dave'+urlpart}></PlayN>
+      <PlayN deletenumber={deletenumber} setDeleteNumber={setDeleteNumber} rescan={rescan} setRescan={setRescan} mp3number={mp3number} setMp3Number={setMp3Number} programnumber={programnumber} setProgramNumber={setProgramNumber} seek={seek} setSeek={setSeek} url={url}></PlayN>
+      <PlayN deletenumber={deletenumber} setDeleteNumber={setDeleteNumber} rescan={rescan} setRescan={setRescan} mp3number={mp3number} setMp3Number={setMp3Number} programnumber={programnumber} setProgramNumber={setProgramNumber} seek={seek} setSeek={setSeek} url={url + '/dave' + urlpart}></PlayN>
     </div>
 
   );
--- src/PlayN.js
+++ src/PlayN.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './PlayN.css'
-export const PlayN = ({deletenumber,setDeleteNumber,rescan,setRescan,mp3number,setMp3Number,programnumber,setProgramNumber,url,seek,setSeek}) => {
-  const setvalue=url.includes('dave');
-  
+export const PlayN = ({ deletenumber, setDeleteNumber, rescan, setRescan, mp3number, setMp3Number, programnumber, setProgramNumber, url, seek, setSeek }) => {
+  const setvalue = url.includes('dave');
+
   console.log(setvalue)
   const [backvalue, setB] = React.useState([]);
   console.log(backvalue)
@@ -42,15 +42,15 @@ export const PlayN = ({deletenumber,setDeleteNumber,rescan,setRescan,mp3number,s
       event.target.value
     );
   };
-  const mp3Select=(event)=>{
-    var file=event.target.value
-    file =file.split('/')[file.split('/').length-1];
+  const mp3Select = (event) => {
+    var file = event.target.value
+    file = file.split('/')[file.split('/').length - 1];
     setMp3Number(file)
-    console.log('mp3 file selected',file)
+    console.log('mp3 file selected', file)
   }
   const getSeek = (event) => {
-    const setit=Math.floor(+event.target.value+0.5)
-   setSeek(setit)
+    const setit = Math.floor(+event.target.value + 0.5)
+    setSeek(setit)
     console.log(
       "Seek Value - ",
       setit
@@ -58,103 +58,108 @@ export const PlayN = ({deletenumber,setDeleteNumber,rescan,setRescan,mp3number,s
   };
   const reScan = (event) => {
     console.log('rescan')
-   setRescan(true);
+    setRescan(true);
   };
-  if(!setvalue){
-  return (
-    <div className='base'>
-    {
-      <center>
-        <select  className='sss'  onChange={mp3Select}>
-          <option>Choose jazzfm mp3 file</option>
-          {
-          backvalue.map((i) => (
-              i.mp3files
-                .map((option, index) => {
-                  return (
-                    <option key={index}>
-                      {option}
-                    </option>
-                  );
-                })
-            ))
-          }
-        </select>
-        <h3 className='sss'>You selected mp3 file {mp3number}</h3>
-      </center>
-    }
-    {
-      <center>
-        <select  className='sss'  onChange={programSelect}>
-          <option>Choose a program</option>
-          {
-            backvalue.map((i) => (
-              i.playlist
-                .map((option, index) => {
-                  return (
-                    <option key={index}>
-                      {option}
-                    </option>
-                  );
-                })
-            ))
-          }
-        </select>
-        <h3 className='sss'>{programnumber==='-1'?'Nothing selected':programnumber}</h3>
-      </center>
-    }
-    {
-      <center>
-        <select  className='sss'  onChange={deleteSelect}>
-          <option>Choose a program to delete</option>
-          {
-            backvalue.map((i) => (
-              i.playlist
-                .map((option, index) => {
-                  return (
-                    <option key={index}>
-                      {option}
-                    </option>
-                  );
-                })
-            ))
-          }
-        </select>
-        <h3 className='sss'>{deletenumber==='-1'?'Nothing selected':deletenumber}</h3>
-      </center>
-    }
-      {
-<center>
-<div>
-<label for="seek">Seek</label>
-  <input onChange={getSeek} type="range" id="seek"  value={seek} step="any" min="0" max="100" />
-  <h3 className='sss'>Play from {seek<=0?"start":seek+"%"}</h3>
-</div>
-</center>
-      }
+  if (!setvalue) {
+    return (
+      <div className='base'>
+        {
+          <center>
+            <select className='sss' onChange={mp3Select}>
+              <option>Choose jazzfm mp3 file</option>
+              {
+                backvalue.map((i) => (
+                  i.mp3files
+                    .map((option, index) => {
+                      return (
+                        <option key={index}>
+                          {option}
+                        </option>
+                      );
+                    })
+                ))
+              }
+            </select>
+            <h3 className='sss'>{mp3number==='-1'?'Nothing selected':'You selected mp3 file '+mp3number}</h3>
+          </center>
+        }
+        {
+          <center>
+            <select className='sss' onChange={programSelect}>
+              <option>Choose a program</option>
+              {
+                backvalue.map((i) => (
+                  i.playlist
+                    .map((option, index) => {
+                      return (
+                        <option key={index}>
+                          {option}
+                        </option>
+                      );
+                    })
+                ))
+              }
+            </select>
+            <h3 className='sss'>{programnumber === '-1' ? 'Nothing selected' : programnumber}</h3>
+          </center>
+        }
+        {
+          <center>
+            <div>
+              <label for="seek">Seek</label>
+              <input onChange={getSeek} type="range" id="seek" value={seek} step="any" min="0" max="100" />
+              <h3 className='sss'>Play from {seek <= 0 ? "the start" : seek + "%"}</h3>
+            </div>
+          </center>
+        }
+
+        {
+          <div>
+            <button className='update' defaultValue={false} onClick={reScan}>Update</button>
+          </div>
+        }
 
 {
-<div>
-  <button className='update' onClick={reScan}>Update</button>
-</div>
-      }
-      
-      {
-        backvalue.map(i=>(
-          <p className='backers'>Current status:<br></br>{i.status}</p>
-        ))
-      }
-    </div>
-  );
-}else{
-  return (
-    <div className='base'>
-      {
-        backvalue.map(i=>(
-          <p className='backers'>New status:<br></br>{i.status}</p>
-        ))
-      }
-    </div>
-  );
-}
+          backvalue.map(i => (
+            <p className='backers'>Current status:<br></br>{i.status}</p>
+          ))
+        }
+        {
+          <center >
+            <select className='sssd' onChange={deleteSelect}>
+              <option>Choose a program to delete</option>
+              {
+                backvalue.map((i) => (
+                  i.playlist
+                    .map((option, index) => {
+                      return (
+                        <option key={index}>
+                          {option}
+                        </option>
+                      );
+                    })
+                ))
+              }
+            </select>
+            <h3 className='sssd'>{deletenumber === '-1' ? 'Nothing selected' : deletenumber}</h3>
+          </center>
+        }
+      </div>
+    );
+  } else {
+    return (
+      <div className='base'>
+        {
+          backvalue.map(i => (
+            <p className='backers'>New status:<br></br>{i.status}</p>
+          ))
+        }
+        {
+          backvalue.map(i => (
+            <p className='backers'>{i.update?'Backup status:':''}<br></br>{i.update}</p>
+          ))
+        }
+      </div>
+    );
+  }
 };
